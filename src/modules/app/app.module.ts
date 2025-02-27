d1dacbf63ce6cb7792a10ac570663bd96d99a880
YuriiVasylyshyn <vasylyshyn.yura.19@gmail.com>
2025-02-27 16:21:37 +0200

    feat: connect db with typeorm

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,7 +1,10 @@
-import { configParser, configSchema } from '@app/config';
+import * as path from 'path';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
+import { Config, configParser, configSchema } from '@app/config';
 import { AppController } from './controllers/app.controller';
 import { AppServiceImpl } from './services/app.service';
 import { AppService } from './services/app.service.abstract';
@@ -16,6 +19,22 @@ const appService = { provide: AppService, useClass: AppServiceImpl };
       envFilePath: '.env',
       load: [configParser],
       validationSchema: configSchema,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService<Config, true>) => {
+        return {
+          type: 'postgres',
+          url: config.get('databaseUrl'),
+          entities: [path.join(__dirname, '..', '**', '*.{entity,view}.{ts,js}')],
+          migrations: [path.join(__dirname, '..', '..', 'migrations/*{.ts,.js}')],
+          migrationsTransactionMode: 'each',
+          migrationsRun: false, //NOTE: Should be 'false' for local environment
+          synchronize: false,
+          namingStrategy: new SnakeNamingStrategy(),
+        } as TypeOrmModuleOptions;
+      },
     }),
   ],
   controllers: [AppController],
