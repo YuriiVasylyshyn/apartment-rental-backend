2c483c2cdca7ad34d6db619748954e3ed5055455
YuriiVasylyshyn <vasylyshyn.yura.19@gmail.com>
2025-03-02 13:07:34 +0200

    feat: add jwt envs

--- src/config/config.interface.ts
+++ src/config/config.interface.ts
@@ -1,4 +1,11 @@
+export type StageType = 'prod' | 'local';
+
 export interface Config {
+  stage: StageType;
   port: number;
   databaseUrl: string;
+  jwt: {
+    secret: string;
+    accessTokenExpirationTimeSeconds: number;
+  };
 }
