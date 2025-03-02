9e72998d91db83f92140c01398bad971fe80db8a
YuriiVasylyshyn <vasylyshyn.yura.19@gmail.com>
2025-03-02 16:19:31 +0200

    feat: update env, install dependencies

--- src/config/config.schema.ts
+++ src/config/config.schema.ts
@@ -3,7 +3,13 @@ import * as Joi from 'joi';
 export const configSchema = Joi.object({
   STAGE: Joi.string().valid('prod', 'local').default('prod'),
   PORT: Joi.number().default(3000),
+
   DATABASE_URL: Joi.string().required(),
+
   JWT_SECRET: Joi.string().required(),
   JWT_ACCESS_TOKEN_EXPIRES_IN: Joi.number().required(),
+
+  AWS_ACCESS_KEY_ID: Joi.string().optional(),
+  AWS_SECRET_ACCESS_KEY: Joi.string().optional(),
+  AWS_REGION: Joi.string().optional(),
 });
